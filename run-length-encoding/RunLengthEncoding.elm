dc5e2a030e2d7f6b722513c8c11a6580dc724504
Tyler Haas <haasdevelopment@gmail.com>
2017-08-30 10:14:57 -0500

    group sequence by chars

--- run-length-encoding/RunLengthEncoding.elm
+++ run-length-encoding/RunLengthEncoding.elm
@@ -1,6 +1,6 @@
 module RunLengthEncoding exposing (decode, encode, version)
 
-import List exposing (head)
+import List exposing (drop, head, length)
 
 
 version : Int
@@ -21,10 +21,32 @@ encode sequence =
         -- Print length of group followed by letter of group
         |> List.foldl
 
-groupSequence : List Char -> List String
-groupSequence chars = 
+
+groupSequence : List Char -> List (List Char)
+groupSequence chars =
     case head chars of
         Just c ->
-            
+            let
+                seq =
+                    takeWhile (\x -> x == c) chars
+
+                rest =
+                    drop (length seq) chars
+            in
+            [ seq ] ++ groupSequence rest
+
         Nothing ->
-            
+            []
+
+
+takeWhile : (a -> Bool) -> List a -> List a
+takeWhile fn items =
+    case items of
+        x :: xs ->
+            if fn x then
+                x :: takeWhile fn xs
+            else
+                []
+
+        _ ->
+            []
